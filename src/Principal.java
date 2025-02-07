6a302fa7b5adbcc1e293be00af6277a582638803
RyoGnim001 <105007789+RyoGnim001@users.noreply.github.com>
2025-02-07 14:42:53 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -4,14 +4,14 @@ public class Principal {
         FolhaDePagamento folha = new FolhaDePagamento();
 
 
-        Empregado emp1 = new Assalariado("João", "Silva", "123.456.789-00", 3000.00);
-        Empregado emp2 = new Comissionado("Maria", "Santos", "987.654.321-00", 10000.00, 0.1);
-        Empregado emp3 = new Horista("Carlos", "Oliveira", "111.222.333-44", 20.00, 160);
+        Empregado empregado1 = new Assalariado("João", "Silva", "123.456.789-00", 3000.00);
+        Empregado empregado2 = new Comissionado("Maria", "Santos", "987.654.321-00", 10000.00, 0.1);
+        Empregado empregado3 = new Horista("Carlos", "Oliveira", "111.222.333-44", 20.00, 160);
 
 
-        folha.adicionarFuncionario(emp1);
-        folha.adicionarFuncionario(emp2);
-        folha.adicionarFuncionario(emp3);
+        folha.adicionarFuncionario(empregado1);
+        folha.adicionarFuncionario(empregado2);
+        folha.adicionarFuncionario(empregado3);
 
 
         System.out.println("Funcionários na Folha de Pagamento:");
